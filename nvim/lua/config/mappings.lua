3020c2bb1ea7f74017fa34279e2249d00c15b57e
Alexander Frolov <fxrlv@icloud.com>
2024-05-22 22:51:02 +0200

    iterm

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -8,3 +8,6 @@
 
 vim.keymap.set('n', '<Leader>w', '<Cmd>w<CR>')
 vim.keymap.set('n', '<Leader>q', '<Cmd>q<CR>')
+
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<S-l>", "<Cmd>bnext<CR>", { desc = "Next buffer" })
