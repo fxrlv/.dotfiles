855a915b93f4a38efc82265d8b38ead08661af40
Alexander Frolov <winningpiece@gmail.com>
2024-12-26 23:33:22 +0100

    lastiterm

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,9 @@ return {
 
     init = function()
       vim.g['jellybeans_overrides'] = {
-        background = { guibg = '151515' },
+        background = { guibg = '121212' },
         StatusLine = { guibg = '262626' },
-        VertSplit = { guibg = '151515' },
+        VertSplit = { guibg = '121212' },
       }
     end,
 
