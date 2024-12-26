7b41d52edd3ccd2777291c9b4b2caa45a2029b79
Alexander Frolov <fxrlv@icloud.com>
2024-12-26 23:33:22 +0100

    lastiterm

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,25 +2,18 @@ return {
   {
     'neovim/nvim-lspconfig',
 
-    dependencies = {
-      "williamboman/mason.nvim",
+    opts = {
+        servers = {
+            gopls = {},
+        },
     },
 
-    config = function()
-      local lspconfig = require('lspconfig')
-      local mason = require("mason")
-
-      mason.setup()
-
-    --lspconfig.gopls.setup({
-    --  settings = {
-    --      gopls = {
-    --          env = {
-    --              GOPACKAGESDRIVER = "/Users/fxrlv/Developer/nebo/nebazel/tools/gopackagesdriver.sh",
-    --          },
-    --      },
-    --  },
-    --})
+    config = function(_, opts)
+        require("lspconfig").gopls.setup({
+            cmd = { "/Users/fxrlv/go/bin/gopls" },
+        })
     end,
+
+    enabled = false,
   },
 }
