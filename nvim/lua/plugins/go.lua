4517e5416d9e0a9fe3e907276a7eefdd79a15b1e
Alexander Frolov <winningpiece@gmail.com>
2025-01-04 23:27:47 +0100

    go

---
+++ nvim/lua/plugins/go.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "go", "gomod", "gowork", "gosum" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              completeUnimported = true,
+              semanticTokens = true,
+              staticcheck = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
