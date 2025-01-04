4530084cef7d4efe979dd63979e1287fdf8e838a
Alexander Frolov <fxrlv@icloud.com>
2025-01-04 19:40:34 +0100

    snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    ---@module "snacks"
+    ---@type snacks.Config
+    opts = {
+      dashboard = { enabled = false },
+      indent = {
+        animate = { enabled = false },
+      },
+    },
+  },
+}
