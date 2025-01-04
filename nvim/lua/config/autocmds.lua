999ab86cd2b9b8befc1bee76524646a34b094398
Alexander Frolov <winningpiece@gmail.com>
2025-01-04 21:42:59 +0100

    tidy

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "go" },
-  callback = function()
-    vim.opt_local.expandtab = false
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = { "go" },
+--   callback = function()
+--     vim.opt_local.expandtab = false
+--     vim.opt_local.shiftwidth = 4
+--     vim.opt_local.tabstop = 4
+--   end,
+-- })
