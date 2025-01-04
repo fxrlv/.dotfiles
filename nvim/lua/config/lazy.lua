33916b563c4c1c88bd3757985c3143860a630047
Alexander Frolov <fxrlv@icloud.com>
2025-01-04 16:23:22 +0100

    disable netrw

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
