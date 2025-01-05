18830c9566f11282f51ba562dde6561952d73678
Alexander Frolov <winningpiece@gmail.com>
2025-01-05 13:12:41 +0100

    bzl

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,17 @@
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "go" },
---   callback = function()
---     vim.opt_local.expandtab = false
---     vim.opt_local.shiftwidth = 4
---     vim.opt_local.tabstop = 4
---   end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "bzl" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go" },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+  end,
+})
