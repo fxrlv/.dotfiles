3a774842fc06ff57ba8322da300a3d4a1e801038
Alexander Frolov <winningpiece@gmail.com>
2025-01-05 12:38:13 +0100

    gitlab

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,6 +8,15 @@ return {
       indent = {
         animate = { enabled = false },
       },
+      gitbrowse = {
+        url_patterns = {
+          ["gitlab%."] = {
+            branch = "/-/tree/{branch}",
+            file = "/-/blob/{branch}/{file}#L{line_start}-L{line_end}",
+            commit = "/-/commit/{commit}",
+          },
+        },
+      },
     },
   },
 }
