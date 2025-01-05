2721f463b50635c75ba5d0b9d3687e50c5f10050
Alexander Frolov <fxrlv@icloud.com>
2025-01-05 23:47:42 +0100

    ui

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        style_preset = 4,
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = "slope",
+      },
+    },
+  },
+}
