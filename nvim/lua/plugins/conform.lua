7c1241d9b7cf2c25c3afe87b4b32a72906df2db2
Alexander Frolov <fxrlv@icloud.com>
2025-04-03 14:53:02 +0200

    lock

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ["*"] = { "trim_newlines", "trim_whitespace" },
+        ["_"] = { "trim_newlines", "trim_whitespace", lsp_format = "last" },
       },
     },
   },
