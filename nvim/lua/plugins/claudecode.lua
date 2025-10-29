4ede2ed1a35f201d87c6c6ad0f16113236b9197e
Alexander Frolov <winningpiece@gmail.com>
2025-10-29 18:10:17 +0100

    claudecode

--- nvim/lua/plugins/claudecode.lua
+++ nvim/lua/plugins/claudecode.lua
@@ -9,13 +9,9 @@ return {
     },
     keys = {
       { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
       { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-      {
-        "<leader>as",
-        "<cmd>ClaudeCodeTreeAdd<cr>",
-        desc = "Add file",
-        ft = { "NvimTree", "neo-tree", "oil", "minifiles" },
-      },
+      -- Diff management
       { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
       { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
